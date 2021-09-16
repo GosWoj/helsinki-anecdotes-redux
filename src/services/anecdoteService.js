f451875f881d34dc07af468b2eac20242b0e6538
GosWoj <gosiawoojcik@gmail.com>
2021-09-16 15:51:01 +0200

    backend step1 done - added fetching anecdotes with json-server

---
+++ src/services/anecdoteService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3001/anecdotes";
+
+const getAll = async () => {
+  const response = await axios.get(baseURL);
+
+  return response.data;
+};
+
+const anecdoteService = {
+  getAll,
+};
+
+export default anecdoteService;
