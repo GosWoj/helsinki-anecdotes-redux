40b90ea1a107a0905e3782c44799b2f001188e41
GosWoj <gosiawoojcik@gmail.com>
2021-09-28 11:50:06 +0200

    backend step6 done - changed action creator for notification

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
-import {
-  setNotification,
-  removeNotification,
-} from "../reducers/notificationReducer";
+import { setNotification } from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(({ anecdotes, filter }) => {
@@ -18,10 +15,7 @@ const AnecdoteList = () => {
 
   const vote = (id, anecdote) => {
     dispatch(addVote(id, anecdote));
-    dispatch(setNotification(`You voted for ${anecdote.content}`));
-    setTimeout(() => {
-      dispatch(removeNotification());
-    }, 5000);
+    dispatch(setNotification(`You voted for ${anecdote.content}`, 10));
   };
 
   return (
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -2,23 +2,23 @@ const notificationReducer = (state = "", action) => {
   switch (action.type) {
     case "SET_NOTIFICATION":
       return action.text;
-    case "REMOVE_NOTIFICATION":
-      return "";
     default:
       return state;
   }
 };
 
-export const setNotification = (text) => {
-  return {
-    type: "SET_NOTIFICATION",
-    text,
-  };
-};
-
-export const removeNotification = () => {
-  return {
-    type: "REMOVE_NOTIFICATION",
+export const setNotification = (text, duration) => {
+  return async (dispatch) => {
+    dispatch({
+      type: "SET_NOTIFICATION",
+      text,
+    });
+    setTimeout(() => {
+      dispatch({
+        type: "SET_NOTIFICATION",
+        text: "",
+      });
+    }, duration * 1000);
   };
 };
 
